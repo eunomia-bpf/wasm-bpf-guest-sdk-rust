5e6478777e8b0b248add25b339bef523f9de35e4
officeyutong <yt.xyxx@gmail.com>
2023-05-30 13:15:36 +0000

    reconstruct & add libbpf-rs-like sdk

---
+++ wasm-bpf-libbpf-rs/src/map.rs
@@ -0,0 +1,103 @@
+use wasm_bpf_binding::binding::wasm_bpf_map_operate;
+
+use crate::{Error, Result};
+
+const BPF_MAP_LOOKUP_ELEM: i32 = 1;
+const BPF_MAP_UPDATE_ELEM: i32 = 2;
+const BPF_MAP_DELETE_ELEM: i32 = 3;
+const BPF_MAP_GET_NEXT_KEY: i32 = 4;
+
+pub struct Map {
+    pub(crate) name: String,
+    pub(crate) fd: i32,
+}
+
+impl Map {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn fd(&self) -> i32 {
+        self.fd
+    }
+    /// Lookup an element in the map. Returns `true` and fill `value_out` if found
+    pub fn lookup(&self, key: &[u8], flags: MapFlags, value_out: &mut [u8]) -> bool {
+        let ret = wasm_bpf_map_operate(
+            self.fd,
+            BPF_MAP_LOOKUP_ELEM,
+            key.as_ptr() as u64,
+            value_out.as_ptr() as u64,
+            0,
+            flags.bits(),
+        );
+        ret != -1
+    }
+    pub fn delete(&self, key: &[u8]) -> Result<()> {
+        let ret = wasm_bpf_map_operate(self.fd, BPF_MAP_DELETE_ELEM, key.as_ptr() as u64, 0, 0, 0);
+        if ret != 0 {
+            Err(Error::FailedToDelete(ret))
+        } else {
+            Ok(())
+        }
+    }
+    pub fn update(&self, key: &[u8], value: &[u8], flags: MapFlags) -> Result<()> {
+        let ret = wasm_bpf_map_operate(
+            self.fd,
+            BPF_MAP_UPDATE_ELEM,
+            key.as_ptr() as u64,
+            value.as_ptr() as u64,
+            0,
+            flags.bits(),
+        );
+        if ret != 0 {
+            Err(Error::FailedToUpdate(ret))
+        } else {
+            Ok(())
+        }
+    }
+    pub fn keys(&self, start_key: &[u8]) -> MapKeyIter {
+        MapKeyIter {
+            fd: self.fd,
+            prev: start_key.to_vec(),
+        }
+    }
+}
+
+bitflags::bitflags! {
+    /// Flags to configure [`Map`] operations.
+    pub struct MapFlags: u64 {
+        /// See [`libbpf_sys::BPF_ANY`].
+        const ANY      = 0;
+        /// See [`libbpf_sys::BPF_NOEXIST`].
+        const NO_EXIST = 1;
+        /// See [`libbpf_sys::BPF_EXIST`].
+        const EXIST    = 2;
+        /// See [`libbpf_sys::BPF_F_LOCK`].
+        const LOCK     = 4;
+    }
+}
+
+pub struct MapKeyIter {
+    fd: i32,
+    prev: Vec<u8>,
+}
+impl Iterator for MapKeyIter {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut next_key = self.prev.clone();
+        let ret = wasm_bpf_map_operate(
+            self.fd,
+            BPF_MAP_GET_NEXT_KEY,
+            self.prev.as_ptr() as u64,
+            0,
+            next_key.as_mut_ptr() as u64,
+            0,
+        );
+        if ret != 0 {
+            None
+        } else {
+            self.prev = next_key.clone();
+            Some(next_key)
+        }
+    }
+}
---
+++ wasm-bpf-libbpf-rs/src/object.rs
@@ -0,0 +1,68 @@
+//!  SPDX-License-Identifier: MIT
+//!
+//! Copyright (c) 2023, eunomia-bpf
+//! All rights reserved.
+//!
+
+use wasm_bpf_binding::binding::{
+    wasm_bpf_map_fd_by_name, wasm_close_bpf_object, wasm_load_bpf_object,
+};
+
+use crate::{map::Map, prog::Program, Error, Result};
+
+#[derive(Default)]
+pub struct ObjectBuilder {}
+
+impl ObjectBuilder {
+    pub fn open_memory<T: AsRef<str>>(&mut self, mem: &[u8]) -> Result<OpenObject> {
+        Ok(OpenObject { buf: mem.to_vec() })
+    }
+}
+
+pub struct OpenObject {
+    buf: Vec<u8>,
+}
+
+impl OpenObject {
+    pub fn load(self) -> Result<Object> {
+        let ptr = &self.buf[..];
+        let ptr = ptr.as_ptr();
+        let handle = wasm_load_bpf_object(ptr as u32, self.buf.len() as i32);
+        if handle == 0 {
+            return Err(Error::FailedToOpen);
+        }
+        Ok(Object { handle })
+    }
+}
+
+pub struct Object {
+    handle: u64,
+}
+
+impl Object {
+    pub fn prog(&self, name: impl AsRef<str>) -> Option<Program> {
+        Some(Program {
+            name: name.as_ref().to_string(),
+            prog_handle: self.handle,
+        })
+    }
+    pub fn map(&self, name: impl AsRef<str>) -> Option<Map> {
+        let mut name_bytes = name.as_ref().as_bytes().to_vec();
+        name_bytes.push(0);
+        let fd = wasm_bpf_map_fd_by_name(self.handle, name_bytes[..].as_ptr() as u32);
+        if fd < 0 {
+            None
+        } else {
+            Some(Map {
+                name: name.as_ref().to_string(),
+                fd,
+            })
+        }
+    }
+}
+
+impl Drop for Object {
+    fn drop(&mut self) {
+        wasm_close_bpf_object(self.handle);
+    }
+}
